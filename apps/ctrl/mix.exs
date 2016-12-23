cd5fab9dae74786b656564df4bc62324b50b6230
Arjan Molenaar <gaphor@gmail.com>
2016-12-23 11:03:06 +0100

    Remove comments

--- apps/ctrl/mix.exs
+++ apps/ctrl/mix.exs
@@ -14,27 +14,11 @@ defmodule Ctrl.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger],
     mod: {Brewberry.Ctrl, [Brewberry.Measure.FakeBackend, Brewberry.Heater.FakeBackend]}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # To depend on another app inside the umbrella:
-  #
-  #   {:myapp, in_umbrella: true}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
   end
