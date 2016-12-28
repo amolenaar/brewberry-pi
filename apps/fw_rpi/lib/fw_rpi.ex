be22a30bb0b0112a6c21224567c376d0dd05ba96
Arjan Molenaar <gaphor@gmail.com>
2016-12-28 09:13:52 +0100

    Set up wifi from application

--- apps/fw_rpi/lib/fw_rpi.ex
+++ apps/fw_rpi/lib/fw_rpi.ex
@@ -7,17 +7,20 @@ defmodule Brewberry.FwRpi do
   @wlan_interface :wlan0
 
   def start(_type, _args) do
-#    System.cmd("modprobe", ["8192cu"])
-#    :timer.sleep(250)
-#    System.cmd("/usr/sbin/wpa_supplicant", ["-s", "-B",
-#        "-i", @wlan_interface,
-#        "-D", "wext",
-#        "-c", "/etc/wpa_supplicant.conf"])
-#    :timer.sleep(500)
-
-#    Networking.setup @wlan_interface
-
+    start_wifi
     {:ok, self}
   end
 
+  def start_wifi do
+    System.cmd("modprobe", ["8192cu"])
+    :timer.sleep(250)
+
+    System.cmd("/usr/sbin/wpa_supplicant", ["-s", "-B",
+        "-i", @wlan_interface,
+        "-D", "wext",
+        "-c", "/etc/wpa_supplicant.conf"])
+    :timer.sleep(500)
+
+    Networking.setup @wlan_interface
+  end
 end
