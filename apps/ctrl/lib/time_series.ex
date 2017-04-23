f5b795ad680bf101cfec18ec005b40c0b605df8a
Arjan Molenaar <gaphor@gmail.com>
2017-04-23 18:18:18 +0200

    Add a small server to keep track of past readings

---
+++ apps/ctrl/lib/time_series.ex
@@ -0,0 +1,58 @@
+defmodule Brewberry.TimeSeries do
+  use GenServer
+  @moduledoc false
+
+  @history_sec 10800 # 3 hours
+  @delay_sec 300 # 5 minutes
+
+  def start_link(name \\ __MODULE__) do
+    GenServer.start_link(__MODULE__, nil, [name: name])
+  end
+
+  def update(time_series \\ __MODULE__, timestamp, value) do
+    GenServer.cast(time_series, {:update, {timestamp, value}})
+  end
+
+  @doc """
+  Obtain a list of series `{timestamp, value}` in ascending order.
+  """
+  def get_series(time_series \\ __MODULE__, since \\ 0) do
+    GenServer.call(time_series, {:series, since})
+  end
+
+  @doc """
+  Truncate the log of sample up back to 3 hours from the most recent sample.
+  """
+  def truncate(time_series \\ __MODULE__) do
+    GenServer.cast(time_series, {:truncate, @history_sec})
+  end
+
+  ## Server
+
+  @doc """
+  Initialize the server with an empty time series and an oldest sample timestamp.
+  """
+  def init(nil) do
+    {:ok, {[], 0}}
+  end
+
+  def handle_cast({:update, {ts, _val}=sample}, {samples, t}) do
+    if ts - @history_sec - @delay_sec >= t, do: truncate(self())
+    {:noreply, {[sample | samples], t}}
+  end
+
+  def handle_cast({:truncate, interval}, {[{ts, _val} | _rest]=samples, oldest_ts}) when ts - interval >= oldest_ts do
+    delete_from_ts = ts - interval
+    {:noreply,
+     {samples
+     |> Enum.take_while(fn ({ts, _val}) -> ts > delete_from_ts end), delete_from_ts}}
+  end
+
+  def handle_cast({:truncate, _time}, state) do
+    {:noreply, state}
+  end
+
+  def handle_call({:series, since}, _from, {samples, _t}=state) do
+    {:reply, samples |> Enum.take_while(fn {t, _s} -> t > since end) |> Enum.reverse, state}
+  end
+end
---
+++ apps/ctrl/test/time_series_test.exs
@@ -0,0 +1,56 @@
+defmodule TimeSeriesTest do
+  use ExUnit.Case
+  @moduledoc false
+
+  alias Brewberry.TimeSeries
+
+  @one_hour 3600
+  @dummy_val 1
+
+  setup do
+    {:ok, pid} = TimeSeries.start_link(:test)
+    {:ok, time_series: pid}
+  end
+
+  test "should store a time series", %{time_series: time_series} do
+    TimeSeries.update(time_series, 1000, %{val: 100})
+    TimeSeries.update(time_series, 2000, %{val: 20})
+
+    assert TimeSeries.get_series(time_series) == [{1000, %{val: 100}}, {2000, %{val: 20}}]
+  end
+
+  test "should get a time series since a start date", %{time_series: time_series} do
+    TimeSeries.update(time_series, 1000, %{val: 100})
+    TimeSeries.update(time_series, 2000, %{val: 20})
+
+    assert TimeSeries.get_series(time_series, 100) == [{1000, %{val: 100}}, {2000, %{val: 20}}]
+    assert TimeSeries.get_series(time_series, 1000) == [{2000, %{val: 20}}]
+    assert TimeSeries.get_series(time_series, 2001) == []
+  end
+
+  test "series should truncate after 3 hours", %{time_series: time_series} do
+    TimeSeries.update(time_series, 1 * @one_hour, @dummy_val)
+    TimeSeries.update(time_series, 2 * @one_hour - 1, @dummy_val)
+    TimeSeries.update(time_series, 2 * @one_hour, @dummy_val)
+    TimeSeries.update(time_series, 2 * @one_hour + 1, @dummy_val)
+    TimeSeries.update(time_series, 3 * @one_hour, @dummy_val)
+    TimeSeries.update(time_series, 5 * @one_hour, @dummy_val)
+
+    wait_until 10, fn -> {:messages, []} = :erlang.process_info(time_series, :messages) end
+
+    assert TimeSeries.get_series(time_series) == [{2 * @one_hour + 1, @dummy_val}, {3 * @one_hour, @dummy_val}, {5 * @one_hour, @dummy_val}]
+  end
+
+  def wait_until(0, fun), do: fun.()
+
+  def wait_until(timeout, fun) do
+    try do
+      fun.()
+    rescue
+      MatchError ->
+        :timer.sleep(10)
+        wait_until(max(0, timeout - 10), fun)
+    end
+  end
+
+end
